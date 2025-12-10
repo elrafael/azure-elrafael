2d79bf00cca396d75c0ca04ab858d6c7a0e0cda7
Rafael V. Oliveira <elrafael123@gmail.com>
2025-12-10 10:40:32 +0000

    feat: change vars and methods names

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,27 +2,27 @@ import axios from "axios"
 import { useState } from "react"
 
 export default function Weather() {
-  const [tempo, setTempo] = useState(null)
+  const [weather, setWeather] = useState(null)
 
-  async function buscarClima() {
+  async function searchWeather() {
     const url =
       "https://api.open-meteo.com/v1/forecast?latitude=38.680&longitude=-9.155&current_weather=true"
     const r = await axios.get(url)
-    setTempo(r.data.current_weather)
+    setWeather(r.data.current_weather)
   }
 
   return (
     <div className="container mt-5">
       <h2>Clima em Almada 🌤</h2>
-      <button className="btn btn-primary mt-3" onClick={buscarClima}>
+      <button className="btn btn-primary mt-3" onClick={searchWeather}>
         Buscar agora
       </button>
 
-      {tempo && (
+      {weather && (
         <div className="alert alert-info mt-3">
-          Temperatura: {tempo.temperature}°C
+          Temperatura: {weather.temperature}°C
           <br />
-          Vento: {tempo.windspeed} km/h
+          Vento: {weather.windspeed} km/h
         </div>
       )}
     </div>
